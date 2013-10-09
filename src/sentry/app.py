24ca73da40015cc0d6e2e7fc9b228e2186725042
David Cramer <dcramer@gmail.com>
2013-10-08 21:51:22 -0700

    Model search backend off nodestore and maintain legacy

--- src/sentry/app.py
+++ src/sentry/app.py
@@ -9,12 +9,13 @@ sentry.app
 from django.conf import settings
 from sentry.utils.imports import import_string
 from threading import local
-from pyelasticsearch import ElasticSearch
 
 
 class State(local):
     request = None
     data = {}
+
+env = State()
 
 
 def get_instance(path, options):
@@ -22,13 +23,6 @@ def get_instance(path, options):
     return cls(**options)
 
 
-def get_search(options):
-    return ElasticSearch(**options)
-
 buffer = get_instance(settings.SENTRY_BUFFER, settings.SENTRY_BUFFER_OPTIONS)
 quotas = get_instance(settings.SENTRY_QUOTAS, settings.SENTRY_QUOTA_OPTIONS)
-env = State()
-if settings.SENTRY_USE_SEARCH:
-    search = get_search(settings.SENTRY_SEARCH_OPTIONS)
-else:
-    search = None
+search = get_instance(settings.SENTRY_SEARCH, settings.SENTRY_SEARCH_OPTIONS)
--- src/sentry/tasks/index.py
+++ src/sentry/tasks/index.py
@@ -6,81 +6,13 @@ sentry.tasks.index
 :license: BSD, see LICENSE for more details.
 """
 
+from __future__ import absolute_import
+
 from celery.task import task
-from collections import defaultdict
-
-# TODO: ensure upgrade creates search schemas
-# TODO: optimize group indexing so it only happens when a group is updated
-# TODO: only index an event after a group is indexed??
-# TODO: confirm replication=async is a good idea
-# TODO: determine TTL
-
-
-def schema_for_event():
-    return {
-        'group': {
-            '_parent': {
-                'type': 'group',
-            }
-        }
-    }
-
-
-def schema_for_group():
-    return {
-    }
-
-
-def document_for_group(group):
-    doc = {
-        'datetime': group.last_seen,
-        'project': group.project.id,
-        'team': group.team.id,
-    }
-
-    return doc
-
-
-def document_for_event(event):
-    doc = defaultdict(list)
-    for interface in event.interfaces.itervalues():
-        for k, v in interface.get_search_context(event).iteritems():
-            doc[k].extend(v)
-
-    doc['text'].extend([
-        event.message,
-        event.logger,
-        event.server_name,
-        event.culprit,
-    ])
-
-    doc.update({
-        'datetime': event.datetime,
-        'project': event.project.id,
-        'team': event.team.id,
-    })
-
-    return doc
 
 
 @task(name='sentry.tasks.index.index_event', queue='search')
 def index_event(event, **kwargs):
-    from sentry.app import app
+    from sentry import app
 
-    group = event.group
-
-    app.search.index(
-        index='sentry',
-        doc_type='group',
-        doc=document_for_group(group),
-        id=group.id,
-        replication='async',
-    )
-    app.search.index(
-        index='sentry',
-        doc_type='event',
-        doc=document_for_event(event),
-        id=event.id,
-        parent=group.id,
-        replication='async',
-    )
+    app.search.index(event)
