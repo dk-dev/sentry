8863ceb1a71636a25146e77e325f3c7a6c1e32ac
David Cramer <dcramer@gmail.com>
2013-10-11 16:31:41 +0100

    remove django-indexer (bad merge)

--- setup.py
+++ setup.py
@@ -68,7 +68,6 @@ install_requires = [
     'Django>=1.4.5,<1.5',
     'django-celery>=3.0.11,<3.1.0',
     'django-crispy-forms>=1.2.3,<1.3.0',
-    'django-indexer>=0.3.0,<0.4.0',
     'django-paging>=0.2.4,<0.3.0',
     'django-picklefield>=0.3.0,<0.4.0',
     'django-static-compiler>=0.3.0,<0.4.0',
